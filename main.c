5a62dbec1a6d61680c11e79bb3acd9f882a50652
mgena <mgena@student.21-school.ru>
2019-10-08 21:04:09 +0300

    tarted to write algorithm

--- main.c
+++ main.c
@@ -74,7 +74,7 @@ u_int64_t    make_tetramino(const char *string)
     return (res);
 }
 
-void get_tetraminos(int fd, u_int64_t tetraminoarr[26])
+int 	get_tetraminos(int fd, u_int64_t tetraminoarr[26])
 {
 	char tetrachr[22];
 	int x;
@@ -84,29 +84,91 @@ void get_tetraminos(int fd, u_int64_t tetraminoarr[26])
 
 	while (read(fd, tetrachr, 21) > 0)
 	{
+		if (x == 27)
+			error();
 		tetraminoarr[x++] = make_tetramino(tetrachr);
+
 	}
 	if (tetrachr[21] == '\n')
 		error();
+	printf( "%d\n", x);
+	return (x);
+}
+
+int sqrt_map(int count)
+{
+	int i;
+
+	i = 1;
+	count *= 4;
+	while (i * i <= count)
+		i++;
+	return (i);
+}
+
+int fillit(u_int64_t tetramins[26], int size_map, u_int16_t map[16]) {
+	uint64_t *tmp;
+	int i;
+	int k;
+
+	i = 0;
+	k = 0;
+	tmp = map;
+	if (tetramins[0] == 0)
+		return (1);
+	while (k <= size_map)
+	{
+		while (i <= size_map)
+		{
+			if ((*(u_int64_t *) (map) & tetramins[0]) != 0)
+				break ;
+			tetramins[0] >>= 1;
+			i++;
+		}
+		if ((*(u_int64_t *) (map) & tetramins[0]) != 0)
+			break ;
+		tetramins[0] <<= i;
+		map++;
+		i = 0;
+		k++;
+	}
+	if (k == size_map + 1 && i == size_map + 1)
+		return (0);
+	*(u_int64_t *) (map) ^= tetramins[0];
+	if (fillit(&tetramins[1], size_map, tmp) == 1)
+	else
+	{
+		if (i + 1 != size_map)
+			fillit((tetramins[0] >>= 1), size_map, tmp);
+		else
+	}
+
+
+
 }
 
 int main(void)
 {
 	int fd;
-	char *line;
 	u_int64_t tetramino_array[26];
 	u_int16_t map[16];
-	int i = 0;
+	int tetramino_count;
+	int size_map;
 
 	fd = open("test", O_RDONLY);
-    get_tetraminos(fd, tetramino_array);
-    while (i != 5)
+	tetramino_count = 0;
+	while (tetramino_count != 26)
+		tetramino_array[tetramino_count++] = 0;
+    tetramino_count = get_tetraminos(fd, tetramino_array);
+	size_map = sqrt_map(tetramino_count);
+	fillit(tetramino_array, size_map, map);
+    while (tetramino_count != -1)
     {
-    	printf("%llu\n", tetramino_array[i]);
-    	print_tetramino((const u_int16_t *) &(tetramino_array[i++]));
+    	printf("%llu\n", tetramino_array[tetramino_count]);
+    	print_tetramino((const u_int16_t *) &(tetramino_array[--tetramino_count]));
     }
 	*(u_int64_t*)(map)  ^= tetramino_array[0];
-	*(u_int64_t*)(map)  ^= tetramino_array[1] >> 1;
+	//*(u_int64_t*)(map)  ^= tetramino_array[1] >> 1;
     print_map(map);
     return 0;
 }
